76b80cb26dff6eb2a23b7d81317a7e7259f40450
Panji Faith Baskoro <panjidia995@gmail.com>
2019-11-03 16:55:15 +0700

    it's not removed, it's just moved to new folder

--- model/delete_item.php
+++
@@ -1,9 +0,0 @@
-<?php
-$id = $_GET['id'];
-
-$query = $db->prepare("DELETE FROM `mmo_item` WHERE idItem= :id");
-$query->bindParam(":id", $id);
-$query->execute();
-// header('location:?mmopilot=manageitem');
-echo "<script type=\"text/javascript\">alert(' Data berhasil dihapus');document.location='?mmopilot=manageitem';</script>"
-?>
--- model/delete_job.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo $id = $_GET['id'];
-
-$query = $db->prepare("DELETE FROM `mmo_job` WHERE idJob= :id");
-$query->bindParam(":id", $id);
-$query->execute();
-// header('location:?mmopilot=manageitem');
-echo "<script type=\"text/javascript\">alert(' Data berhasil dihapus');document.location='?mmopilot=managejob';</script>"
-?>
