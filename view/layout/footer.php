5b4fb0a3591402d03023d05984259ef303da44dc
Panji Faith Baskoro <panjidia995@gmail.com>
2019-11-08 01:26:48 +0700

    menambahkan js untuk select cascade

--- view/layout/footer.php
+++ view/layout/footer.php
@@ -1,7 +1,68 @@
 
 </div>
 <!-- /Body Wrap  -->
-
+<script>
+var stateObject = {
+    "Jawa Timur": {
+        "JTM-1": ["Kabupaten Banyuwangi","Kota Batu","Kabupaten Bondowoso","Kabupaten Jember","Kabupaten Lumajang","Kota Malang","Kabupaten Malang"],
+        "JTM-2": []
+      },
+    "Bali":{
+      "LPT-1": ["Kabupaten Gianyar","Kabupaten Bali","Kabupaten Buleleng","Kabupaten Karangasem"]
+    },
+    "Nusa Tenggara Barat (NTB)": {
+      "LPT-1": ["Kota Bima","Kabupaten Bima","Kabupaten Dompu","Kota Mataram"]
+    },
+    "Nusa Tenggara Timur (NTT)": {
+      "LPT-1":["Kabupaten Alor","Kabupaten Ende","Kabupaten Ndori","Kabupaten Ende"]
+    }
+}
+window.onload = function () {
+    var stateSel = document.getElementById("provinsi"),
+        countySel = document.getElementById("area"),
+        citySel = document.getElementById("kokab");
+    for (var state in stateObject) {
+        stateSel.options[stateSel.options.length] = new Option(state, state);
+    }
+    stateSel.onchange = function () {
+        countySel.length = 1; // remove all options bar first
+        citySel.length = 1; // remove all options bar first
+        if (this.selectedIndex < 1) {
+          countySel.options[0].text = "Please select state first"
+          citySel.options[0].text = "Please select county first"
+          return; // done   
+        }  
+        countySel.options[0].text = "Please select county"
+        for (var county in stateObject[this.value]) {
+            countySel.options[countySel.options.length] = new Option(county, county);
+        }
+        if (countySel.options.length==2) {
+          countySel.selectedIndex=1;
+          countySel.onchange();
+        }  
+        
+    }
+    stateSel.onchange(); // reset in case page is reloaded
+    countySel.onchange = function () {
+        citySel.length = 1; // remove all options bar first
+        if (this.selectedIndex < 1) {
+          citySel.options[0].text = "Please select county first"
+          return; // done   
+        }  
+        citySel.options[0].text = "Please select city"
+        
+        var cities = stateObject[stateSel.value][this.value];
+        for (var i = 0; i < cities.length; i++) {
+            citySel.options[citySel.options.length] = new Option(cities[i], cities[i]);
+        }
+        if (citySel.options.length==2) {
+          citySel.selectedIndex=1;
+          citySel.onchange();
+        }  
+        
+    }
+}
+</script>
 <!-- Scripts -->
 
 <!-- jQuery -->
@@ -60,6 +121,8 @@
 
 <!-- Select2 JS -->
 <script type="text/javascript" src="view/appp.js"></script>
+
+<!-- wizard -->
 <!-- Typeahead JS -->
 
 <!-- TagManager JS -->
