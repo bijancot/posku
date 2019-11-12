9955f02fb8841ee8c123feee9a7ce969455feafe
Panji Faith Baskoro <panjidia995@gmail.com>
2019-11-12 08:43:00 +0700

    dynamic js

--- test.php
+++ test.php
@@ -1,9 +1,22 @@
 <?php
+require "conn/koneksi.php";
 
+$select = $_POST['selected'];
 
-$string = "Kabupaten Malang";
+$kolo = explode(" ",$select);
 
-$kolo = explode(" ",$string);
+$kokab = $kolo[0];
+$kokabname = $kolo[1]." ".$kolo[2];
 
-echo $kolo[0];
+$query = $db->prepare("SELECT * from kode_area where cityorkab=:cityorkab and citykabname=:citykabname");
+$query->bindParam(":cityorkab",$kokab);
+$query->bindParam(":citykabname",$kokabname);
+$query->execute();
+
+$hasil = $query->fetchAll();
+
+foreach ($hasil as $key) {
+    echo "<h5>Area yang dipilih : ".$key['area']."</h5>";
+}
+
 ?>
